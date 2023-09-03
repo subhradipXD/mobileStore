e7bd3c63de0fadc2fb44094fa2d7c7e90948ad0d
Subhradip <subhradipdas6969@gmail.com>
2023-09-03 23:22:36 +0530

    cart

--- User/cart.php
+++ User/cart.php
@@ -2,6 +2,9 @@
 // Include database connection code here
 include("../_dbConnect.php");
 
+echo "Reached Cart.php";
+
+
 // session_start(); // Start the session if it hasn't been started already
 
 if (!isset($_SESSION['user_id'])) {
@@ -12,32 +15,27 @@ if (!isset($_SESSION['user_id'])) {
 
 $user_id = $_SESSION['user_id'];
 
-// Generate a random cart ID
-$randomNumber = (string)mt_rand(100, 999);
-$cart_id = $user_id . $randomNumber;
+// Check if a cart already exists for the user
+$cartQuery = "SELECT cartid FROM cart WHERE userid = '$user_id'";
+$result = mysqli_query($conn, $cartQuery);
 
-// Insert the cart ID and user ID into the cart table
-$cartQuery = "INSERT INTO cart (cartid, userid) VALUES ('$cart_id', '$user_id')";
-
-if (mysqli_query($conn, $cartQuery)) {
-    // The cart has been created successfully, now you can proceed with adding products to the cart.
+if (mysqli_num_rows($result) > 0) {
+    // If a cart already exists, retrieve the cart ID
+    $row = mysqli_fetch_assoc($result);
+    $cart_id = $row['cartid'];
     echo '<form action="addToCart.php" method="get">';
 } else {
-    echo 'Error creating the cart.';
+    // If a cart doesn't exist, generate a new cart ID
+    $randomNumber = (string)mt_rand(100, 999);
+    $cart_id = $user_id . $randomNumber;
+
+    // Insert the cart ID and user ID into the cart table
+    $insertCartQuery = "INSERT INTO cart (cartid, userid) VALUES ('$cart_id', '$user_id')";
+
+    if (mysqli_query($conn, $insertCartQuery)) {
+        // The cart has been created successfully, now you can proceed with adding products to the cart.
+        echo '<form action="addToCart.php" method="get">';
+    } else {
+        echo 'Error creating the cart.';
+    }
 }
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-
-<body>
-
-</body>
-
-</html>
