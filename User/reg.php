c28279e94e8a3a128d76c3e802ada7d6c37a6475
Subhradip <subhradipdas6969@gmail.com>
2023-09-11 23:37:02 +0530

    nthng

--- User/reg.php
+++ User/reg.php
@@ -68,6 +68,7 @@ if (isset($_POST['Submit'])) {
 
 <head>
 	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>UserRegistration</title>
 	<link rel="stylesheet" type="text/css" href="reg.css">
 
